a5cb1ecb45107be880dcf54aeb760c9334674ca2
lgwk42 <luisgunkim@gmail.com>
2024-11-19 15:33:29 +0900

    :tada: :: 프로젝트 생성

---
+++ src/test/java/com/project/everytime/EveryTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.everytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
