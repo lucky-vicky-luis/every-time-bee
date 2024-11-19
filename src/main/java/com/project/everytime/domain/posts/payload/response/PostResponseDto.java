c14cccbe8d4c555253c7206dd555b9c2d2bb21a3
최미래 <miraechoi@dgsw.hs.kr>
2024-11-19 20:26:47 +0900

    feature: add post response dto

---
+++ src/main/java/com/project/everytime/domain/posts/payload/response/PostResponseDto.java
@@ -0,0 +1,16 @@
+package com.project.everytime.domain.posts.payload.response;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record PostResponseDto(
+        Long id,
+        String title,
+        String content,
+        LocalDateTime regDate,
+        Long likeCount,
+        boolean public_status
+) {
+}
