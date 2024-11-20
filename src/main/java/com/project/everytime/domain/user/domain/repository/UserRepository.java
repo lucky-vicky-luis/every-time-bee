d69c87039c35b078df6426c3e20ea2bb97633c76
최미래 <miraechoi@dgsw.hs.kr>
2024-11-20 14:16:40 +0900

    fix: fix post repository id type

--- src/main/java/com/project/everytime/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/project/everytime/domain/user/domain/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.project.everytime.domain.user.domain.entity.UserEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 }
