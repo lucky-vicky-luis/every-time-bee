3e927530088ddf8f2a8db0c581e6e398ab87bdaa
lgwk42 <luisgunkim@gmail.com>
2024-11-23 21:48:08 +0900

    fix: api url 수정

--- src/main/java/com/project/everytime/domain/comment/client/api/CommentController.java
+++ src/main/java/com/project/everytime/domain/comment/client/api/CommentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
