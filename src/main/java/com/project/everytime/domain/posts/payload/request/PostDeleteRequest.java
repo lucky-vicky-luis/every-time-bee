a878d92510bc9ad94a7091bb04d496027de8f631
lgwk42 <luisgunkim@gmail.com>
2024-11-26 11:04:49 +0900

    fix: 유효성 검사 추가

--- src/main/java/com/project/everytime/domain/posts/payload/request/PostDeleteRequest.java
+++ src/main/java/com/project/everytime/domain/posts/payload/request/PostDeleteRequest.java
@@ -1,7 +1,11 @@
 package com.project.everytime.domain.posts.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PostDeleteRequest(
+        @NotBlank
         Long postId,
+        @NotBlank
         Long writerId
 ) {
 }
--- src/main/java/com/project/everytime/domain/posts/payload/request/PostSearchRequest.java
+++ src/main/java/com/project/everytime/domain/posts/payload/request/PostSearchRequest.java
@@ -1,14 +1,15 @@
 package com.project.everytime.domain.posts.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PostSearchRequest {
-
     private int page;
     private int size;
+    @NotBlank
     private String title;
 
 }
